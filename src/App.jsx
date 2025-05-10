0f3b401e36759e5aed5f129f29a51080d56c37cf
nahashon <nashonmwendwa0@gmail.com>
2025-05-10 23:33:04 +0300

    created files and folders and added basic code to the app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
-import { Routes, Route } from "react-router-dom";
-import Navbar from './components/Navbar';
-import HomePage from './pages/Homepage'; // Make sure your file name is correct
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import ClientRoutes from "./routes/ClientRoutes";
+import AdminRoutes from "./routes/AdminRoutes";
+import AuthRoutes from "./routes/AuthRoutes";
+import { AuthProvider } from "./contexts/AuthContext";
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </div>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          {/* Client Pages */}
+          <Route path="/*" element={<ClientRoutes />} />
+
+          {/* Admin Pages */}
+          <Route path="/admin/*" element={<AdminRoutes />} />
+
+          {/* Authentication Pages */}
+          <Route path="/auth/*" element={<AuthRoutes />} />
+
+          {/* Redirect unknown routes to home */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
